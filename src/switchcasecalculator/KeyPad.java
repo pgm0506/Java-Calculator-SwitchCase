bf3fb1ca8ce1fa1abd4e4ab1537d2b20c08be83b
pgm0506 <pgm050612@gmail.com>
2023-05-03 01:24:23 +0900

    GUI의 기능 추가

--- src/switchcasecalculator/KeyPad.java
+++ src/switchcasecalculator/KeyPad.java
@@ -9,15 +9,19 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 // 소스만 입력하고 Ctrl+Shift+O를 누른다. 
 public class KeyPad extends JFrame implements ActionListener {
 	private JPanel panel1;
-	private String temp = "";
-
+	private JTextArea txt;
+	String islem;
+	int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE;
 	public KeyPad() {
 		
+		txt = new JTextArea(5,40);
+		add(txt, BorderLayout.NORTH);
 		panel1 = new JPanel();
 		panel1.setLayout(new GridLayout(3, 3));
 		add(panel1, BorderLayout.CENTER);
@@ -27,19 +31,65 @@ public class KeyPad extends JFrame implements ActionListener {
 			btn.setPreferredSize(new Dimension(100, 30));
 			panel1.add(btn);
 		}
+		
 		pack();
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		txt.setText("************************************\n"
+				+ "1. 더하기\n" + "2. 빼기\n" + "3. 곱하기\n" + "4. 나누기\n" +
+				"************************************");
 	}
-
+		
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String actionCommand = e.getActionCommand();
-		String result = "";
+		if(islem == null || islem.isEmpty() ) {
+			islem = actionCommand;
+		}
+		else if(a == Integer.MAX_VALUE)
+			a = Integer.parseInt(actionCommand);
+		else if(b == Integer.MAX_VALUE)
+			b = Integer.parseInt(actionCommand);
 		
-		
-		
-		
+		switch (islem) {
+		case "1":
+			if(a == Integer.MAX_VALUE)
+				txt.setText("첫번째 수: ");
+			else if(b == Integer.MAX_VALUE) 
+				txt.setText(txt.getText() + actionCommand + ("\n두번째 수: "));
+			else
+				txt.setText(txt.getText() + actionCommand + "\n계산 결과 : " + (a + b));
+			break;
+		case "2":
+			if(a == Integer.MAX_VALUE)
+				txt.setText("첫번째 수: ");
+			else if(b == Integer.MAX_VALUE) 
+				txt.setText(txt.getText() + actionCommand + ("\n두번째 수: "));
+			else
+				txt.setText(txt.getText() + actionCommand + "\n계산 결과 : " + (a - b));
+			break;
+		case "3":
+			if(a == Integer.MAX_VALUE)
+				txt.setText("첫번째 수: ");
+			else if(b == Integer.MAX_VALUE) 
+				txt.setText(txt.getText() + actionCommand + ("\n두번째 수: "));
+			else
+				txt.setText(txt.getText() + actionCommand + "\n계산 결과 : " + (a * b));
+			break;
+		case "4":
+			if(a == Integer.MAX_VALUE)
+				txt.setText("첫번째 수: ");
+			else if(b == Integer.MAX_VALUE) 
+				txt.setText(txt.getText() + actionCommand + ("\n두번째 수: "));
+			else
+				txt.setText(txt.getText() + actionCommand + "\n계산 결과 : " + (a / b));
+			break;
+			
+		default:
+			txt.setText("잘못 선택하셨습니다 프로그램을 종료합니다");
+		}
+
 	}
+
+}
 	
-	}
--- src/switchcasecalculator/SwitchCaseCalculator.java
+++ src/switchcasecalculator/SwitchCaseCalculator.java
@@ -1,67 +1,22 @@
 package switchcasecalculator;
+
 import java.util.Scanner;
 
-
-
-
-
-
-
-
-
 public class SwitchCaseCalculator {
-public static void main(String[] args) {
-	KeyPad f = new KeyPad();
+	public static String islem = "";
 
+	public static void main(String[] args) throws InterruptedException {
+		KeyPad f = new KeyPad();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("************************************");
+		String islemler = "1. 더하기\n" + "2. 빼기\n" + "3. 곱하기\n" + "4. 나누기";
+		System.out.println(islemler);
+		System.out.println("************************************");
+		System.out.println("연산할 방식 선택:");
+		Thread.sleep(100000);
+		int a;
+		int b;
 
-Scanner scanner = new Scanner(System.in);
-        System.out.println("************************************");
-        String islemler = "1. 더하기\n"
-                          +"2. 빼기\n"
-                          +"3. 곱하기\n"
-                          +"4. 나누기";
-        System.out.println(islemler);
-        System.out.println("************************************");
-        System.out.println("연산할 방식 선택:");
-        String islem = scanner.nextLine();
-        int a;
-        int b;
-        
-        switch(islem){
-            case "1":
-                System.out.print("첫번째 수: ");
-                a = scanner.nextInt();
-                System.out.print("두번째 수: ");
-                b = scanner.nextInt();
-                System.out.println("계산 결과 : " + (a+b));
-                break;
-            case "2":
-                System.out.print("첫번째 수: ");
-                a = scanner.nextInt();
-                System.out.print("두번째 수: ");
-                b = scanner.nextInt();
-                System.out.println("계산 결과 : " + (a-b));
-                break;
-            case "3":
-                System.out.print("첫번째 수: ");
-                a = scanner.nextInt();
-                System.out.print("두번째 수: ");
-                b = scanner.nextInt();
-                System.out.println("계산 결과 : " + (a * b));
-                break;
-            case "4":
-                System.out.print("첫번째 수: ");
-                a = scanner.nextInt();
-                System.out.print("두번째 수: ");
-                b = scanner.nextInt();
-                System.out.println("계산 결과 : " + ((double)a / b));
-                break;
-            default:
-                System.out.println("잘못 선택하셨습니다 프로그램을 종료합니다");
-            }
-        
-        
-        
-        }
-    
+	}
+
 }
