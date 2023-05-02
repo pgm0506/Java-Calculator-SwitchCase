c35d15e34b8ea1a00b0e67413a38bb0facbbbd0d
pgm0506 <pgm050612@gmail.com>
2023-05-03 01:36:51 +0900

    오타수정

--- src/switchcasecalculator/SwitchCaseCalculator.java
+++ src/switchcasecalculator/SwitchCaseCalculator.java
@@ -7,16 +7,6 @@ public class SwitchCaseCalculator {
 
 	public static void main(String[] args) throws InterruptedException {
 		KeyPad f = new KeyPad();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("************************************");
-		String islemler = "1. 더하기\n" + "2. 빼기\n" + "3. 곱하기\n" + "4. 나누기";
-		System.out.println(islemler);
-		System.out.println("************************************");
-		System.out.println("연산할 방식 선택:");
-		Thread.sleep(100000);
-		int a;
-		int b;
-
 	}
 
 }
